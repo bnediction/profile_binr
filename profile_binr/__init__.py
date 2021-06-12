4efe90c717fffa134d48edaa9023787be902c4bb
Gustavo Magaña López <gmaganna.biomed@gmail.com>
2021-06-12 10:58:41 +0200

    :sparkles: Added normalisation functions.
    These can be used to process raw counts and obtain the log-normalised data that is used by most analyses.

--- profile_binr/__init__.py
+++ profile_binr/__init__.py
@@ -3,6 +3,7 @@ The PROFILE methodology for the binarisation and normalisation of RNA-seq data.
 """
 
 from .wrappers.probinr import ProfileBin
+from .utils.normalization import log_transform, normalize, log_normalize
 
 __version__ = "0.1.0"
 __author__ = "Gustavo Magaña López"
